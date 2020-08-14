1e0c1eff6dc2f456548ebfc93e49050f162ebdc2
konny0311 <tatsuya.kono.dev@gmail.com>
2020-08-14 12:27:17 +0900

    fn ch4 ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-    println!("the length of {} is {}", s1, len);
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", word);
 }
 
-fn calculate_length(s: &String) -> usize{
-    s.len()
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
