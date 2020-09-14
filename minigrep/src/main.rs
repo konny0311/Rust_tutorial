920ee7708186da8d89cee46e85ddc5e8664b607d
konny0311 <tatsuya.kono.dev@gmail.com>
2020-09-14 20:48:09 +0900

    fin ch12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,7 +5,7 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
